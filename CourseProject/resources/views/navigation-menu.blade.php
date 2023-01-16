779ca3e5ac49232ad4823045a0397dc3e6bdb35d
Todor Petrov <todorpetrov55@gmail.com>
2023-01-16 21:11:17 +0200

    Navigation menu
    Added main routes to the navigation menu.

--- CourseProject/resources/views/navigation-menu.blade.php
+++ CourseProject/resources/views/navigation-menu.blade.php
@@ -5,15 +5,33 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="/">
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-jet-nav-link href="{{ route('animes') }}" :active="request()->routeIs('animes')">
+                        {{ __('Animes') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('characters') }}" :active="request()->routeIs('characters')">
+                        {{ __('Characters') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('vas') }}" :active="request()->routeIs('vas')">
+                        {{ __('Voice Actors') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="/admin" :active="request()->routeIs('admin')">
+                        {{ __('Admin Panel') }}
                     </x-jet-nav-link>
                 </div>
             </div>
